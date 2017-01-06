bc2373363aaa3a960fc426dcb212d3549ea2159e
William Dubosclard <william.dubosclard@gmail.com>
2017-01-06 12:53:20 +0100

    interface_graphique_TKinter2.0
    Mise en place de la classe Interface Graphique
    Mise en place des boutons et de leurs fonctionnements
    
    A Terminer :
    - Intégrer une fenêtre Matplotlib à l'interface (pour l'instant le graphe apparait dans la console ipython)
    - Changer d'environnement pour passer de TKinter à une bibliothèque plus élaborée.

--- interface_graphique_TKinter.py
+++ interface_graphique_TKinter.py
@@ -1,5 +1,12 @@
 # -*- coding: utf-8 -*-
 """
+Created on Fri Jan 06 10:56:47 2017
+
+@author: maitre
+"""
+
+# -*- coding: utf-8 -*-
+"""
 Created on Thu Jan 05 19:48:23 2017
 
 @author: DragoMagnus
@@ -9,43 +16,102 @@ Interface Graphique TKinter - Projet
 """
 
 import Tkinter as tk
-import matplotlib
+import numpy as np
+import matplotlib.pyplot as plt
 
-fenetre = tk.Tk()
-label = tk.Label(fenetre, text="Interface ADUC").pack()
+class Interface_Graphique(tk.Tk):
+    """
+    Cette classe permet la configuration de l'interface graphique
+    """
+    
+    def __init__(self,parent):
+        """
+        La fonction init contient l'initialisation de la fenêtre graphique
+        De plus un paramètre parent est implémenté. Ce paramètre sert de base
+        aux différents widgets (boutons, graphes) qui seront implémentés
+        dans la suite.
+        """
+        tk.Tk.__init__(self,parent)
+        self.parent = parent ### Garde en memoire la reference parent
+        self.interface_graphique()
+        self.mainloop() ### Permet une boucle infinie afin que la fenetre
+                        ### de l'interface ne disparait pas
 
-
-"""
-Case à cocher qui indique le port.
-Si la case est coché alors le port est ouvert, dans le cas contraire le port
-est fermé. Un message sera envoyé
-"""
-open_port = tk.Checkbutton(fenetre, text="Port USB").pack(side=tk.LEFT)
-
-
-"""
-Case qui permet d'entrer le numero de port usb auquel est
-connecté la carte ADUC
-"""
-usb_value = tk.StringVar()
-usb_value.set("Entrer le port USB")
-entry_usb = tk.Entry(fenetre, textvariable=usb_value, width=30).pack(side=tk.LEFT)
-
-
-"""
-Création des boutons START et STOP qui
-permettrons le lancement ou l'arret de l'aquisition des données
-"""
-start_button = tk.Button(fenetre, text="START", command=fenetre.quit).pack(side=tk.RIGHT, padx=5, pady=5)
-stop_button = tk.Button(fenetre, text="STOP", command=fenetre.quit).pack(side=tk.RIGHT, padx=5, pady=5)
-
-
-
-"""
-Création d'une fenêtre qui permettra d'afficher le plot
-de données de la carte ADUC
-"""
-tk.Canvas(fenetre, width=250, height=50, bg='black').pack(side=tk.BOTTOM, padx=5, pady=5)
-
-
-fenetre.mainloop() 
+    def interface_graphique(self):
+        """
+        Cette fonction a pour but de générer les élements de l'interface
+        graphique. Elle contient tous les widgets nécessaires à la 
+        réalisation de l'interface ADUC
+        """
+        self.grid() ### Creation d'une grille. Cette grille facilite la mise
+                    ### en place des differents elements de l'interface
+        
+        """
+        open_port est un widget qui va permettre l'ouverture ou la fermeture
+        du port usb lié à la carte d'aquisition.
+        """
+        self.open_port = tk.Checkbutton(self, text="Port USB") ### Creation
+        self.open_port.grid(column = 0, row = 0, sticky = 'EW') ### Mise en place dans le grid
+        
+        """
+        - usb_value correspond numero du port usb auquel est connecté
+        la carte d'aquisition ADUC
+        - entry_usb est une fenêtre de l'interface graphique dans laquelle
+        on peut entrer le port usb en question et qui donnera la valeur
+        à usb_value
+        """
+        global usb_value
+        usb_value = tk.StringVar() ### Variable string qui autorise les lettres
+        usb_value.set("Entrer le port USB") ### Donne la valeur
+        
+        self.entry_usb = tk.Entry(self, textvariable=usb_value, width=30)
+        self.entry_usb.grid()
+        
+        
+        """
+        button_start et button_stop sont les deux boutons qui permettrons
+        la prise d'aquisition de la carte ADUC
+        """
+        self.button_start = tk.Button(self, text="START", command=self.start)
+        self.button_stop = tk.Button(self, text="STOP", command=self.stop)
+        self.button_start.grid() ### Mise en place des boutons dans le grid
+        self.button_stop.grid()
+        
+        """
+        Mise en place d'une fenêtre Matplot
+        """
+        self.plot = tk.Button(self, text="Graphe", command=self.plot_aquisition)
+        self.plot.grid(column = 3, row = 0)
+        """
+        Mise en place d'un canevas pour y déposer le graphe matplot
+        """
+        global figure
+        figure = tk.Canvas(self, width=250, height=50)
+        figure.grid(column=3, row=1)
+              
+    def start(self):
+        """
+        Cette fonction va permettre l'event start
+        """
+        print 'Aquisition START'
+        
+    def stop(self):
+        """
+        Cette fonction va permettre l'event stop
+        """
+        print 'Aquisition STOP'
+        
+    def plot_aquisition(self):
+        x = np.arange(0, 5, 0.1);
+        y = np.sin(x)
+        plt.plot(x,y)
+        
+        
+    
+        
+        
+        
+graphique = Interface_Graphique(None)
+graphique.interface_graphique
+graphique.entry_usb
+graphique.plot
