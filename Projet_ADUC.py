0d784a5d9c8334c04aa72ce3d18064797fb053fb
titusfranz <titus.franz@web.de>
2017-01-11 14:22:26 +0100

    update of project aduc file inclusive transform of ascii to ndarray

--- Projet_ADUC.py
+++ Projet_ADUC.py
@@ -10,13 +10,16 @@ Programme Projet
 """
 
 import serial as serial
+import numpy as np
+from itertools import izip #in python 3 not needed, replace here izip by zip
 
-class ADUC(serial.Serial):
+class ADUC():
     """
     Classe principale qui va permettre :
     - Free Run
     - Stop
     """
+    def
 
     def stop(self):
         """
@@ -39,8 +42,20 @@ class ADUC(serial.Serial):
         print self.read(1)
         while modus == 'f':
             print self.read(1)
-            print self.read(512)
-
+            ascii =  self.read(512)
+            print ascii
+            asciitoint(ascii)
+    
+    def asciitoint(ascii):
+        raw = iter(map(ord, ascii)) #creates a list of all ASCII-numbers, 
+        # eg from R?:! to [62, 96, 21, 99, 74, 80] and transforms it to an
+        # iteratable object
+        data = np.array([x*100 + y for x,y in izip(raw,raw)] # zips the list to a list of tuples,
+        # e.g [62, 96, 21, 99, 74, 80] to [(62, 96),(21, 99), (74, 80)] and
+        # transforms each tuple to one number, eg [6296, 2199, 7480]
+        # note: in python 3 izip may be replaced by zip
+        
+        
 carte = ADUC()
 carte.port = 'COM3'
 carte
