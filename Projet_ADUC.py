3f3763b1edc5bde5f5fb783bd8a1ac53d5f4d18e
William Dubosclard <william.dubosclard@gmail.com>
2017-01-11 19:31:24 +0100

    Interface_Graphique + Projet_ADUC
    - Liens entre l'interface et le projet effectué
    - L'interface peut résoudre tous les problèmes simples,
    - Communication avec la carte ADUC + Conversion des ASCII en Chiffres
    
    
    Problème :
    - l'interface graphique ne peut pas lancer un plot déroulant pour l'instant mais peut (voir historique) lancer un plot simple dans la fenêtre
    - l'interface graphique ne s lance qu'une fois sur deux. Il faut redémarrer le noyau à chaque utilisation ? Problème à voir également !

--- Projet_ADUC.py
+++ Projet_ADUC.py
@@ -13,23 +13,32 @@ import serial as serial
 import numpy as np
 from itertools import izip #in python 3 not needed, replace here izip by zip
 
-class ADUC():
+import pylab
+from pylab import *
+
+
+class ADUC(serial.Serial):
     """
     Classe principale qui va permettre :
     - Free Run
     - Stop
     """
-    def
-
+    
+    """
+    def __init__(self):
+        self.ser = serial
+    """
+    
     def stop(self):
         """
         On définit la fonction stop qui va stopper l'aquisition des données 
         de la carte ADUC
         """
         global modus
-        modus = 's'
+        modus = False
         self.write('s')
         print self.read(1)
+
         
     def freerun(self):
         """
@@ -37,39 +46,44 @@ class ADUC():
         des données de la carte ADUC
         """
         global modus
-        modus = 'f'
+        modus = True
         self.write('f')
         print self.read(1)
-        while modus == 'f':
+        while modus :
             print self.read(1)
-            ascii =  self.read(512)
+            ascii = self.read(512)
             print ascii
-            asciitoint(ascii)
-    
-    def asciitoint(ascii):
-        raw = iter(map(ord, ascii)) #creates a list of all ASCII-numbers, 
-        # eg from R?:! to [62, 96, 21, 99, 74, 80] and transforms it to an
-        # iteratable object
-        data = np.array([x*100 + y for x,y in izip(raw,raw)] # zips the list to a list of tuples,
-        # e.g [62, 96, 21, 99, 74, 80] to [(62, 96),(21, 99), (74, 80)] and
-        # transforms each tuple to one number, eg [6296, 2199, 7480]
-        # note: in python 3 izip may be replaced by zip
-        
-        
-carte = ADUC()
-carte.port = 'COM3'
-carte
-print carte.name
+            self.asciitoint(ascii)
 
-carte.baudrate = 115200
+    def asciitoint(self, ascii):
+        raw = iter(map(ord, ascii)) 
+        """       
+        creates a list of all ASCII-numbers, 
+        eg from ASCII to (62, 96, 21, 99, 74, 80) and transforms it to an
+        iteratable object
+        """
+        data = np.array([x*100 + y for x,y in izip(raw,raw)]) 
+        return data
+        """      
+        zips the list to a list of tuples,
+        e.g (62, 96, 21, 99, 74, 80) to ((62, 96),(21, 99), (74, 80)) and
+        transforms each tuple to one number, eg (6296, 2199, 7480)
+        note: in python 3 izip may be replaced by zip
+        """
 
-carte.open()
-print carte.is_open
 
-# carte.freerun()
+#carte = 0
+#carte = ADUC()
+#carte.port = 'COM3'
+#carte
 
-carte.stop()
 
-carte.close()
-print carte.is_open
+#carte.open()
+#print carte.is_open
 
+#carte.freerun()
+
+#carte.stop()
+
+#carte.close()
+#print carte.is_open
--- interface_graphique_PyQt4.py
+++ interface_graphique_PyQt4.py
@@ -14,8 +14,16 @@ import sys
 from PyQt4 import QtGui ### Module d'interface graphique
 from PyQt4 import QtCore
 
+from pyqtgraph.Qt import QtGui, QtCore
+import pyqtgraph as pg
+
+
+import Projet_ADUC as ADUC
+
 from pylab import*
 from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
+
+carte = ADUC.ADUC()
 
 
 class Interface_Graphique(QtGui.QWidget):
@@ -45,31 +53,38 @@ class Interface_Graphique(QtGui.QWidget):
         ################################################################
         self.button_start = QtGui.QPushButton('START', self) ### Bouton START
         
-        self.button_stop = QtGui.QPushButton('STOP', self) ### Bouton STOP
+        self.button_stop = QtGui.QPushButton('APOCALYPSE', self) ### Bouton STOP
         
-        self.check_port = QtGui.QCheckBox(self) ### Case d'ouverture de port
-        self.check_port.setChecked(False) ### Initialise le Check               
+        self.check_port = QtGui.QPushButton('Ouverture PORT', self) ### Bouton d'ouverture
+        self.check_port_close = QtGui.QPushButton('Fermeture PORT', self) ### Bouton de fermeture
+        """
+        On tentera ici de faire une case check pour remplacer les deux boutons
+        et ainsi n'avoir qu'une seule fonction avec un if
+        """
+        #self.check_port = QtGui.QCheckBox(self) ### Case d'ouverture de port
+        #self.check_port.setChecked(False) ### Initialise le Check               
         
-        self.entry_port = QtGui.QLineEdit("PORT USB") ### Case pour entrer le port
+        self.entry_port = QtGui.QLineEdit("Enter Port") ### Case pour entrer le port
         
         
         self.figure = plt.figure()
-        self.canvas_plot = FigureCanvas(self.figure) ### Insertion d'une case figure
+        self.canvas_plot = pg.GraphicsLayoutWidget() #self.figure) ### Insertion d'une case figure
         
         ################################################################
         ############ POSITION DES WIDGETS ############
         ################################################################
         
+        global grid
         grid = QtGui.QGridLayout() ### Ouverture de la grid (matrice)
         self.setLayout(grid)
         
         grid.addWidget(self.button_start, 0,1)
-        grid.addWidget(self.button_stop, 0,2)
-        grid.addWidget(self.check_port, 1,1)
+        grid.addWidget(self.button_stop, 4,1)
+        grid.addWidget(self.check_port, 0,2)
+        grid.addWidget(self.check_port_close, 1, 1)
         grid.addWidget(self.entry_port, 1,2)     
         grid.addWidget(self.canvas_plot, 3, 1)
         
-
         
         self.show()
         
@@ -80,10 +95,15 @@ class Interface_Graphique(QtGui.QWidget):
         self.button_start.clicked.connect(self.button_start_command)
         self.button_stop.clicked.connect(self.button_stop_command)
         
+        self.check_port.clicked.connect(self.open_port)        
+        self.check_port_close.clicked.connect(self.close_port)
+        
+        self._active = False
+        
         ### Cliquer sur la case va appeler la fonction open_port
         ### ouvrant ou fermant ainsi le port usb de la carte ADUC
         ### Ne fonctionne pour l'instant PAS
-        self.check_port.connect(self.check_port, QtCore.SIGNAL('stateChanged(int'), self.open_port)
+        #self.check_port.connect(self.check_port, QtCore.SIGNAL('stateChanged(int'), self.open_port)
 
         
         ################################################################
@@ -94,51 +114,108 @@ class Interface_Graphique(QtGui.QWidget):
         Cette fonction affiche le message de lancement de l'aquisition
         et lance l'aquisition de la carte ADUC
         """
-        ### Ajouter les commandes disponibles dans le programme principal
-        ### pour la commande START, ie :
-        ### - carte.freerun()
-        x = np.linspace(0., 4*np.pi, 100)
-        plt.plot(x, np.sin(x))
-        show()
-        print "Aquisition START"     
+        if not self._active :
+            self._active = True
+            self.button_start.setText("STOP")
+            self.scrollplot()
+            QtCore.QTimer.singleShot(0, self.freerun)
+            print "Aquisition START"
+        else :
+            self._active = False
+            self.button_start.setText("START")
+            QtCore.QTimer.singleShot(0, self.button_stop_command)
+            print "Aquisition STOP"
 
     def button_stop_command(self):
         """
         Cette fonction stop l'aquisition de la carte ADUC et affiche
-        le message d'arrêt
+        le message d'arrêt, cette fonction peut êtr obsolète si la fonction
+        précédente fonctionne.
         """
-        ### Ajouter les commandes disponibles dans le programme principal
-        ### pour la commande STOP, ie :
-        ### - carte.close()
-        ### - carte.is_close() instead of print "Aquisition STOP"
+        carte.stop()
         print "Aquisition STOP"
         
     def open_port(self):
         """
         Cette fonction permet d'ouvrir le port USB lié à la carte ADUC
-        et envoie un message de confirmation d'ouverture
+        et envoie un message de confirmation d'ouverture. Renvoie un
+        TRUE si la commande fonctionne
         """
-        ### - carte.open()
-        ### - carte.is_open() instead of print "Port USB - Open"        
-        if self.check_port.isChecked() :
-            self.lineEdit.setText("Port USV - Open")
-        else :
-            self.lineEdit.setText("Port  USB - Close")
-
-    def port_value(self):
-        """
-        Cette fonction permet de donné au port la valeur à laquelle
-        il peut lire la carte ADUC
-        """
-        ### Remplacer usb_value par carte.port = " "
-        ### Cela permettra de donner la valeur correspondante en changeant le
-        ### pc sur lequel est branche la carte ADUC
-        usb_value = QtGui.QTableWidget(self.entry_port)
-        print usb_value
+        print "Ouverture du port"
         
+        carte.port = self.entry_port.text()
+        print carte.port, carte.name
+        carte.baudrate = 115200
+        
+        carte.open()
+        print carte.is_open
+
+    def close_port(self):
+        """
+        Cette fontion va fermer le port de la carte d'aquisition et vérifier
+        en renvoyant un FALSE que la commande a été effectué
+        """
+        print "Fermeture du port"
+        carte.close()
+        print carte.is_open
+        
+        
+        
+    ################################################################
+    ############ FONCTIONS UTILES ############
+    ################################################################ 
+    def freerun(self):
+        from time import sleep
+        carte.write('f')
+        print carte.read(1)
+        sleep(0.05)        
+        while self._active:
+            print carte.name
+            QtGui.qApp.processEvents()
+            print carte.read(1)
+            ascii = carte.read(512)
+            print carte.asciitoint(ascii)
+            self.data = carte.asciitoint(ascii)
+            
+            if self._active == False:
+                break
+            
+        carte.stop()        
+        self.button_start.setText('START')
+        self._active = False
+        
+    def scrollplot(self):
+        win = self.canvas_plot.GraphicsWindow()
+        win.setWindowTitle('Graphe ... peut etre')
+        
+        p1 = win.addPlot()
+        data1 = self.data
+        curve1 = p1.plot(data1)
+        ptr1 = 0
+        def update1():
+            global data1, curve1, ptr1
+            data1[:-1] = data1[1:]  # shift data in the array one sample left
+                            # (see also: np.roll)
+            data1[-1] = np.random.normal()
+            curve1.setData(data1)
+        
+        timer = pg.QtCore.QTimer()
+        timer.timeout.connect(update1)
+        timer.start(50)
 
 
 if __name__ == "__main__":
     system = QtGui.QApplication(sys.argv)
     graphique = Interface_Graphique()
+    graphique.show()
     sys.exit(system.exec_())
+    
+    
+    
+"""
+   #if self.check_port.isChecked() :
+         #   self.lineEdit.setText("Port USB - Open")
+          #  print "Port USB - Open"
+        #else :
+          #  self.lineEdit.setText("Port  USB - Close")
+"""
